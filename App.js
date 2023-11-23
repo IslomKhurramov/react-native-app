385a94e7596a873e235f127811b6865481f22c72
IslomKhurramov <islom.khurramov@gmail.com>
2023-11-24 02:25:19 +0900

    BRR: making Loader

---
+++ App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Loader from "./components/loader";
+
+export default function App() {
+  return <Loader />;
+}
+
+const styles = StyleSheet.create({});
